da3375e7f32169e24badde4c966f18437a0e2397
Vladislavus1 <113709712+Vladislavus1@users.noreply.github.com>
2024-07-07 13:35:27 +0300

    commit

--- OlxPyApi/parser.py
+++ OlxPyApi/parser.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from exceptions import *
+from .exceptions import *
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
